3d8c9e91ea30e03ba0864c345ef9c98203e9295c
root <tt970303652@sina.com>
2018-10-24 19:03:16 +0800

    更新文件

--- crm/settings.py
+++ crm/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'login_detail',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -72,8 +73,8 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',  # 数据库连接方式
         'NAME': 'crm',  # 数据库名
         'USER': 'Index',  # 数据库连接用户名
